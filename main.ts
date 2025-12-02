42f46bf44305b14514bd2f4038981c0ffb2f85f6
Gerard Jalón <gerard.jalon@gmail.com>
2025-12-02 23:03:49 +0100

    repte extra

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber > NumLocal) {
+        basic.showIcon(IconNames.Sad)
+    } else if (receivedNumber == NumLocal) {
+        basic.showIcon(IconNames.Asleep)
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
+})
 input.onButtonPressed(Button.A, function () {
-    radio.sendString("\"Hello World\"")
-    basic.showString("Sent!")
+    NumLocal = randint(1, 6)
+    basic.showNumber(NumLocal)
+    radio.sendNumber(NumLocal)
 })
-radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
-})
+let NumLocal = 0
 radio.setGroup(1)
+NumLocal = 0
